86355b19a3cade3fdf047d9c72f0ce20c7b155f6
Serany <17505625+jmctune@users.noreply.github.com>
2023-09-15 16:10:28 -0500

    Adds a plugin to check for weird spacing.

---
+++ custom_checks/space_checker/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="space_checker",
+    version="1.0.0",
+    author="Serany",
+    author_email="dqxtranslationproject@gmail.com",
+    description="Checks to ensure consistent spacing throughout a string.",
+    license="GPLv3+",
+    keywords="Bytes",
+    packages=["space_checker"],
+)
